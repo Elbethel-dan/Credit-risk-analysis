d683681799afc9493fbe61e535b74ccf1ebd3c10
Elbdan1 <99385539+Elbdan1@users.noreply.github.com>
2026-02-23 10:04:12 +0300

    Update the best model path

--- src/api/main.py
+++ src/api/main.py
@@ -22,7 +22,7 @@ def clean_feature_names(X: pd.DataFrame) -> pd.DataFrame:
 
 
 # Path to your model
-MODEL_PATH = os.path.join("/Users/elbethelzewdie/Downloads/credit-risk-analysis/Credit-risk-analysis/model", "LogisticRegression_best_model1.pkl")
+MODEL_PATH = os.path.join("/Users/elbethelzewdie/Downloads/credit-risk-analysis/Credit-risk-analysis/models", "XGBoost_best_model.pkl")
 
 # Load the model
 try:
